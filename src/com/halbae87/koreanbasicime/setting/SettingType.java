95d3398b34c1f358c7a0010453f219a69e69ea04
Hong, SeongChan <seongchan116@gmail.com>
2013-12-29 15:41:31 +0900

    주석 추가 (별다른 의미는 없음)

--- src/com/halbae87/koreanbasicime/setting/SettingType.java
+++ src/com/halbae87/koreanbasicime/setting/SettingType.java
@@ -1,5 +1,14 @@
+/**
+ * Copyright (C) 2013 Hong.SeongChan.
+ **/
+
 package com.halbae87.koreanbasicime.setting;
 
+/**
+ * 설정값 정의
+ * @author Hong,SeongChan
+ *
+ */
 public class SettingType {
 	public static String HANKEYBOARD = "HANKEYBOARD";
 	public static int HAN_SW_DANMOUM = 0;
--- src/com/halbae87/koreanbasicime/setting/Settings.java
+++ src/com/halbae87/koreanbasicime/setting/Settings.java
@@ -1,3 +1,6 @@
+/**
+ * Copyright (C) 2013 Hong.SeongChan.
+ */
 package com.halbae87.koreanbasicime.setting;
 
 import com.halbae87.koreanbasicime.R;
@@ -7,6 +10,10 @@ import android.content.Context;
 import android.content.SharedPreferences;
 
 
+/**
+ * @author Hong,SeongChan
+ *
+ */
 public class Settings {
    	private SharedPreferences mPrefs;
 	private SharedPreferences.Editor mPrefEdit;
@@ -14,15 +21,22 @@ public class Settings {
 	private int mHWHanKeyboard;
 	private Context mContext;
 
-	
+	/**
+	 * Settings Class Contstructor.
+	 * @param context
+	 */
 	public Settings(Context context) {
 		mContext = context;
 		mPrefs = context.getSharedPreferences("setting", Activity.MODE_PRIVATE);
 		mSWHanKeyboard = mPrefs.getInt(mContext.getString(R.string.key_sw_keyboard), SettingType.HAN_SW_DANMOUM);
 		mHWHanKeyboard = mPrefs.getInt(mContext.getString(R.string.key_hw_keyboard), SettingType.HAN_HW_SEBUL);
-		
 	}
 	
+	/**
+	 * 키보드 설정값을 Preference에 저장한다. 
+	 * @param type
+	 * @param value
+	 */
 	public void setHanKeyboard(String type, int value) {
 		
     	mPrefs = mContext.getSharedPreferences("setting", Context.MODE_PRIVATE);
@@ -32,12 +46,21 @@ public class Settings {
 		mPrefEdit.commit();
 	}
 	
+	/**
+	 * 키보드 설정값을 반환한다. 
+	 * int getHanKeyboard()
+	 * @param type
+	 * @return
+	 */
 	public int getHanKeyboard(String type) {
 		if (type.equals(mContext.getString(R.string.key_sw_keyboard))) {
 			return mSWHanKeyboard;
-		} else {
+		} else if (type.equals(mContext.getString(R.string.key_hw_keyboard))) {
+			
 			return mHWHanKeyboard;
-		}
+		} 
+		
+		return 0;
 	}
 	
 }
