c9aac9b29d2cf292d71fb5eb0d97f9752473030d
Hong, SeongChan <seongchan116@gmail.com>
2013-12-29 15:41:59 +0900

    주석 추가 TAG값 변

--- src/com/halbae87/koreanbasicime/Utils/Logs.java
+++ src/com/halbae87/koreanbasicime/Utils/Logs.java
@@ -1,10 +1,18 @@
+/**
+ * Copyright (C) 2013 Hong.SeongChan.
+ **/
+
 package com.halbae87.koreanbasicime.Utils;
 
 import android.util.Log;
 
+/**
+ * Logs Class. (로그출력 관리용 wrapper)
+ * @author Hong,SeongChan
+ */
 public class Logs {
-	static String TAG = "KOREA_IME";
-	static boolean DEBUG = true;
+	static String TAG = "KOREANBASIC_IME";
+	static boolean DEBUG = true;	// 로그를 출력하지 않으려면, false로 변경한다.
 	
 	
 	public static void v(String msg) {
